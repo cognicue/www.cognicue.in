b64924557a9ef59cd52b0c149b659b38a4785f14
akbaruddin <akbaruddin@live.in>
2022-06-22 14:27:38 +0530

    recognition section add

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -164,4 +164,37 @@
         $tooltip.css("visibility", "visible");
         setTimeout(()=>{$tooltip.hide(500);}, 2000);
     });
+    $("#recognitionSlide").slick({
+        lazyLoad: 'ondemand',
+        prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        speed: 1000,
+        autoplay: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                arrow: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrow: false
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrow: false
+            }
+        }]
+    });
 })(jQuery);
