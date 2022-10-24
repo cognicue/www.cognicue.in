54a3619619f3778feefd56a3564789a8bbb7166b
akbaruddin <akbaruddin@live.in>
2022-10-24 20:01:44 +0530

    contact page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
       fontFamily: {
-        'cognicue': ['CogniCue', 'sans-serif']
+        'cognicue': ['CogniCue', 'sans-serif'],
+        'poppins': ['Poppins', ...defaultTheme.fontFamily.sans]
       },
       colors: {
         'primary': '#04dbec',
@@ -12,5 +15,7 @@ module.exports = {
       }
     },
 	},
-	plugins: [],
+	plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
