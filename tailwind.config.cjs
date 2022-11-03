90b34fb017a0d586b84572db26fe368306a8d57e
akbaruddin <akbaruddin@live.in>
2022-11-04 01:04:28 +0530

    facs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './data/*.json'
+  ],
 	theme: {
 		extend: {
       fontFamily: {
